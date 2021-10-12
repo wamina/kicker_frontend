d4dd4b781542096b4186239a2d2aefdd1e61653e
wamina <amina.w@gmx.net>
2021-10-12 14:16:01 +0200

    get data

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let data = [];
 let game = 0;
+
+function getPlayers() {
+    const Http = new XMLHttpRequest();
+    const url = 'http://localhost:8000/players/';
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        if (Http.responseText) {
+            let response = JSON.parse(Http.responseText);
+            let bigData = response.data;
+            data = [];
+            bigData.forEach(function(element) {
+                data.push(element.attributes);
+
+                let titleArray = element.attributes.title.split(", ");
+                element.attributes.title = titleArray;
+            });
+            console.log(data);
+            showGenre();
+        }
+    }
+}
