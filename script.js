48cd58a511281263955444b8c7d7d3d06eeb242d
wamina <amina.w@gmx.net>
2021-10-15 11:41:32 +0200

    add forms

--- script.js
+++ script.js
@@ -7,11 +7,20 @@ function getPlayers() {
     Http.send();
 
     Http.onreadystatechange = (e) => {
-        if (Http.responseText) {
+        if (Http.responseText && playersData.length === 0) {
             let response = JSON.parse(Http.responseText);
             playersData = response.data;
             console.log(playersData);
-            createPlayersRows();
+            if (window.location.pathname === "/players.html") {
+                createPlayersRows();
+            }
+            if (window.location.pathname === "/index.html") {
+                createPlayersDropdown1();
+                createPlayersDropdown2();
+                createPlayersDropdown3();
+                createPlayersDropdown4();
+            }
+
 
         }
     }
@@ -32,4 +41,117 @@ function createPlayersRows() {
 
 
     })
+}
+
+function openForm() {
+    document.getElementById("newGame").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("newGame").style.display = "none";
+}
+
+
+function createPlayersDropdown1() {
+    console.log(playersData);
+
+
+    var select = document.createElement("select");
+    select.name = "Spieler 1";
+    select.id = "player1"
+
+
+
+    playersData.forEach(player => {
+        console.log(player);
+
+        var option = document.createElement("option");
+        option.value = player.attributes.name;
+        option.text = player.attributes.name;
+        select.appendChild(option);
+
+    })
+    var label = document.createElement("label");
+    label.innerHTML = "Spieler 1"
+    label.htmlFor = "player1";
+
+    document.getElementById("container1").appendChild(select);
+}
+
+function createPlayersDropdown2() {
+    console.log(playersData);
+
+
+    var select = document.createElement("select");
+    select.name = "Spieler 2";
+    select.id = "player2"
+
+
+
+    playersData.forEach(player => {
+        console.log(player);
+
+        var option = document.createElement("option");
+        option.value = player.attributes.name;
+        option.text = player.attributes.name;
+        select.appendChild(option);
+
+    })
+    var label = document.createElement("label");
+    label.innerHTML = "Spieler 2"
+    label.htmlFor = "player2";
+
+    document.getElementById("container2").appendChild(select);
+}
+
+function createPlayersDropdown3() {
+    console.log(playersData);
+
+
+    var select = document.createElement("select");
+    select.name = "Spieler 3";
+    select.id = "player3"
+
+
+
+    playersData.forEach(player => {
+        console.log(player);
+
+        var option = document.createElement("option");
+        option.value = player.attributes.name;
+        option.text = player.attributes.name;
+        select.appendChild(option);
+
+    })
+    var label = document.createElement("label");
+    label.innerHTML = "Spieler 3"
+    label.htmlFor = "player3";
+
+    document.getElementById("container3").appendChild(select);
+}
+
+function createPlayersDropdown4() {
+    console.log(playersData);
+
+
+    var select = document.createElement("select");
+    select.name = "Spieler 4";
+    select.id = "player4"
+
+
+
+    playersData.forEach(player => {
+        console.log(player);
+
+        var option = document.createElement("option");
+        option.value = player.attributes.name;
+        option.text = player.attributes.name;
+        select.appendChild(option);
+
+    })
+    var label = document.createElement("label");
+    label.innerHTML = "Spieler 4"
+    label.htmlFor = "player4";
+
+    document.getElementById("container4").appendChild(select);
 }
